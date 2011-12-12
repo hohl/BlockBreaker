ba8e9d6fd1e1ccf631884dee8d6f07f96d37ad27
Joshy <j_semlitsch@gmx.net>
2011-12-12 17:12:09 +0100

    .

--- Blockbreaker/Blockbreaker/Game1.cs
+++ Blockbreaker/Blockbreaker/Game1.cs
@@ -81,7 +81,7 @@ namespace Blockbreaker
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+            GraphicsDevice.Clear(Color.Pink);
 
             // TODO: Add your drawing code here
 
